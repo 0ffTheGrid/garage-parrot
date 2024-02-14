54d4d8c16e779e2c545e8624f9145efd28c79282
Ludiche <ludichediella01@gmail.com>
2024-02-15 00:45:33 +0100

    home pages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,6 +2,11 @@ import Route from "./Route.JS";
 
 //Routes 
 export const allRoutes = [
-    new Route("/", "Acueil", "/pages/home.html"),];
+    new Route("/", "Acueil", "/pages/home.html"),
+    new Route("/admin", "admin", "/pages/admin.html"),
+    new Route("/emp", "emp", "/pages/emp.html"),
+
+
+];
 
     export const websiteName = "Garage V.Parrot";
